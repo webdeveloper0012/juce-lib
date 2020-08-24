605840036d313aa8cf65bf222be0d87a27f4e64f
Web Developer <60765936+webdeveloper0012@users.noreply.github.com>
2020-08-24 00:49:16 -0300

    Update SamplerPluginDemo.h

--- examples/Plugins/SamplerPluginDemo.h
+++ examples/Plugins/SamplerPluginDemo.h
@@ -16,7 +16,7 @@
 
   ==============================================================================
 */
-
+ 
 /*******************************************************************************
  The block below describes the properties of this PIP. A PIP is a short snippet
  of code that can be read by the Projucer and used to generate a JUCE project.
